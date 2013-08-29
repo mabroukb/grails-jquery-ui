5ba127082f0a64dab59018d18e2f66932c43cff0
mabroukb <mabroukb@gmail.com>
2013-08-29 15:46:28 +0200

    updated to jquery 1.10.3

--- JqueryUiGrailsPlugin.groovy
+++ JqueryUiGrailsPlugin.groovy
@@ -1,31 +1,77 @@
 class JqueryUiGrailsPlugin {
-    // @todo This MUST be updated when you update the version of jquery ui
-    static JQUERYUI_VERSION = "1.8.16"
-
-    // Put in here the minor revision of this plugin, appended to the JQUERY UI version automatically to
-    // make plugin release version
-    static PLUGIN_MINOR_REVISION = ""
 
     // the plugin version
-    // NOTE: this does not compile correctly under Grails 2.0, use 1.3.7
-    def version = JQUERYUI_VERSION + (PLUGIN_MINOR_REVISION ? '.' + PLUGIN_MINOR_REVISION : '')
-
+    // JQUERY-UI-VERSION
+    def version =  "1.10.3"
+    // or JQUERY-UI-VERSION.PLUGIN_MINOR_REVISION
+    // def version =  "1.10.3.1"
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2 > *"
+    def grailsVersion = "2.2 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [jquery:'1.3.2.1 > *']
+    def dependsOn = [jquery:'1.6 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+        "grails-app/views/error.gsp"
     ]
 
-    def author = "Marc Palmer"
-    def authorEmail = "marc@grailsrocks.com"
-    def title = "jQuery UI resources"
+    def author = "mabroukb"
+    def authorEmail = ""
+    def title = "jQuery UI Plugin"
     def description = '''Simply supplies jQuery UI resources, depends on jQuery plugin. Use this plugin to avoid resource duplication and conflicts.'''
 
-    def documentation = "http://grails.org/plugin/jquery-ui"
-    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPJQUERYUI" ]
-    def scm = [ url: 'https://github.com/gpc/grails-jquery-ui']
+    // URL to the plugin's documentation
+    // TODO
+    //    def documentation = "http://grails.org/plugin/jquery-ui"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def license = "GPL2"
+
+    // Details of company behind the plugin (if there is one)
+    //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+
+    // Any additional developers beyond the author specified above.
+    //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+    //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: 'https://github.com/mabroukb/grails-jquery-ui']
+    // PREVIOUS VERSION :
+    //    def documentation = "http://grails.org/plugin/jquery-ui"
+    //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPJQUERYUI" ]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
 }
